f191f970359292a8f05792ac37e3b488b9cd310b
villeravn <villeravn@gmail.com>
2024-01-19 13:56:20 +0100

    temporary shit fordi den der variabelen må skiftas

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let joinbtn = document.getElementById("join");
 let hosttext = document.getElementById("temptext");
 let codefielddiv = document.getElementById("codefielddiv");
 
+var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
 hostbtn.addEventListener("click",function(){
     hostbtn.style.display = "none"
     joinbtn.style.display = "none"
@@ -14,3 +16,15 @@ joinbtn.addEventListener("click",function(){
     joinbtn.style.display = "none"
     codefielddiv.style.display="block"
 })
+
+function randchar (length) {
+    var looplength = length-1
+    var kevin = Math.floor(Math.random() * characters.length);
+    for (let i = 0; i < looplength; i++) {
+        numb = Math.floor(Math.random() * characters.length);        
+        kevin = kevin + characters[numb];
+    }
+    return kevin
+}
+
+document.getElementById("temptext").innerHTML = randchar(6);
