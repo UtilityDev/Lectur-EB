75494ba00d7fffdf2a6919c0f247f447ca3437cd
Julian ⚡ <45518053+UtilityDev@users.noreply.github.com>
2024-02-13 15:21:27 +0100

    Move source files to /src

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
     <div class="main-content">
         <h1 class="title">Lectur</h1>
         <div class="main-buttons">
-            
             <button class="btn blue" id="host"><i class="fa-solid fa-plus"></i> Host Room</button>
             <button class="btn blue" id="join"><i class="fa-solid fa-bolt"></i> Join Room</button>
             <p id="codeText" style="display: none">code</p>
@@ -22,8 +21,8 @@
             </div>
         </div>
     </div>
-    <script src="script.js"></script>
-    <script src="background.js"></script>
+    <script src="src/script.js"></script>
+    <script src="src/background.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/p5.min.js" integrity="sha512-uaz5GpnQoE6t5echKlX8P52czvsIGgLPcvlzfvRubLZ1Hp8JemUDnbUiAahbVtPb+jUVrNETuXvAhDDF/N3M4w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
