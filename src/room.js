04cbf7d2da4f72431a085a1023a85a823e8247a8
Julian ⚡ <45518053+UtilityDev@users.noreply.github.com>
2024-02-14 22:58:32 +0100

    Implement ability to copy room code to the clipboard

--- src/room.js
+++ src/room.js
@@ -3,11 +3,18 @@
 
 let codeText = document.querySelector(".room_code");
 let nickText = document.querySelector(".nick_text");
+let roomCodeText = document.querySelector(".room_code");
 
 const urlParams = new URLSearchParams(window.location.search);
 
 const roomCode = urlParams.get("code");
 const nick = urlParams.get("nick");
+
+// Write room code to clipboard
+roomCodeText.addEventListener("click", function () {
+    navigator.clipboard.writeText(roomCodeText.innerText);    
+    alert("Code copied to clipboard!");
+});
 
 const DOMRefresh = () => {
     codeText.textContent = roomCode;
--- src/room.php
+++ src/room.php
@@ -6,13 +6,13 @@
     <title>Empty Room - Lectur</title>
     <link rel="stylesheet" href="../styles/room.css">
     <link rel="stylesheet" href="../styles/icons.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />    
 </head>
 <body>
     <div class="top-container">
         <!-- <span class="leave_icon"><</span> -->
-        <span class="leave_icon material-symbols-outlined icon-align">arrow_back_ios_new</span>
-        <span class="header_text">Room <span class="room_code">[ROOM_CODE]</span><span> 🤩</span></span>
+        <span class="leave_icon material-symbols-rounded icon-align">arrow_back_ios_new</span>
+        <span class="header_text">Classroom: <span class="room_code">[ROOM_CODE]</span></span>
         <span class="nick_text">[NICK_NAME]</span>
     </div>
 
